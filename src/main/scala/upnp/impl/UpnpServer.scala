acf1d2108287a0153d1a565cb8523c619e120e22
esipeng <steven1983>
2016-04-06 17:01:30 +0800

    update

--- src/main/scala/upnp/impl/UpnpServer.scala
+++ src/main/scala/upnp/impl/UpnpServer.scala
@@ -1,6 +1,6 @@
 package upnp.impl
 
-import java.net.{InetAddress, MulticastSocket}
+import java.net.{DatagramPacket, InetAddress, MulticastSocket}
 
 import akka.actor.Actor.Receive
 import akka.actor.{Actor, ActorLogging, ActorRef}
@@ -12,18 +12,33 @@ import config.ApplicationConfig
 final case class UpnpServiceFound(description:String)
 class UpnpServer(val deviceManager:ActorRef) extends Actor with ActorLogging{
 
+  final case class UpnpMessageReeived(datagramPacket: DatagramPacket)
+
   private val UDP_BUFFER_SIZE = 1024
   private val TIME_TO_LIVE = 4
-
   private val multiSocket = createMultiSocket()
+  private var receiveQuit = false
 
+  private val receiveThread = new Thread(new Runnable {
+    override def run(): Unit = {
+      val datagramPacket = new DatagramPacket(new Array[Byte](UDP_BUFFER_SIZE),UDP_BUFFER_SIZE)
+      while(receiveQuit == false) {
+        multiSocket.receive(datagramPacket)
+        val udpMessage = datagramPacket.clone()
+        UpnpServer.this.self ! UpnpMessageReeived(datagramPacket)
+      }
+    }
+  })
   override def preStart(): Unit = {
     super.preStart()
-
+    receiveThread.start()
   }
 
 
-  override def postStop(): Unit = super.postStop()
+  override def postStop(): Unit ={
+    super.postStop()
+    receiveQuit = true
+  }
 
   def receive = {
 
